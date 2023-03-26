ac4aef8fa667b23fb512ab0cd24c653bcb22ac38
NoamLevi1 <noamlei@mta.ac.il>
2023-03-26 09:05:06 +0300

    diamon class

---
+++ Ex01_02/Diamond.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Ex01_02
+{
+    class Diamond
+    {
+        private int m_DiamondSize;
+
+        public Diamond(int i_DiamondSize)
+        {
+            m_DiamondSize = i_DiamondSize;
+        }
+
+        private static void spaces(int numberOfSpaces)
+        {
+            if (numberOfSpaces == 0)
+            {
+                return;
+            }
+
+            Console.Write(" ");
+            spaces(numberOfSpaces - 1);
+        }
+
+        private static void asterisks(int numberOfAsterisks)
+        {
+            if (numberOfAsterisks == 0)
+            {
+                return;
+            }
+
+            Console.Write("* ");
+            asterisks(numberOfAsterisks - 1);
+        }
+
+        private static void trinagle(int rowNumber, int totalRowsNumber)
+        {
+            if (rowNumber == 0)
+            {
+                return;
+            }
+
+            spaces(rowNumber - 1);
+            asterisks(totalRowsNumber - rowNumber + 1);
+            Console.WriteLine("");
+            trinagle(rowNumber - 1, totalRowsNumber);
+        }
+
+        private static void reverseTrinagle(int rowNumber, int totalRowsNumber)
+        {
+            if (rowNumber == 0)
+            {
+                return;
+            }
+
+            spaces(totalRowsNumber - rowNumber + 1);
+            asterisks(rowNumber - 1);
+            Console.WriteLine("");
+            reverseTrinagle(rowNumber - 1, totalRowsNumber);
+        }
+
+        public void Draw()
+        {
+            trinagle(m_DiamondSize, m_DiamondSize);
+            reverseTrinagle(m_DiamondSize, m_DiamondSize);
+        }
+    }
+}
---
+++ Ex01_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Ex01_02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int size = getUserInput();
+        Diamond userDiamond = new Diamond(size);
+        userDiamond.Draw();
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    private static int getUserInput()
+    {
+        Console.Write("Enter the size of the diamond: ");
+        int size = int.Parse(Console.ReadLine());
+        return size;
+    }
+
+   
+}
+
+        
